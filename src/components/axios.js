38ea1d0431a6ccae5007a85a3b956d17cabc3be6
artufelii <artufelii@DESKTOP-N6G5J8K.localdomain>
2021-08-26 16:23:28 -0500

    Cambios para despliegue

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,15 +1,16 @@
 import axios from 'axios'
 
-let url 
+//let url 
 
-if (process.env.NODE_ENV === 'development') {
-  url = 'http://localhost:5000'
-} else if(process.env.NODE_ENV === 'production') {
-  url = 'https://farmaciajesus-api.herokuapp.com'
-}
+//if (process.env.NODE_ENV === 'development') {
+  //url = 'http://localhost:5000'
+//} else if(process.env.NODE_ENV === 'production') {
+  //url = 'https://farmaciajesus-api.herokuapp.com'
+//}
 
 const instance = axios.create({
-  baseURL: url
+  baseURL: 'https://farmaciajesus-api.herokuapp.com'
+  //baseURL: url
 })
 
 export default instance
