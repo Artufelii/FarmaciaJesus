d09c056f493abf161f8a71a8b151e08c86d8e605
Arturo Alvarez <artufelii@gmail.com>
2022-08-21 23:59:45 -0500

    se realizan cambios en la funcionalidad del menu de filtros

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,26 +6,17 @@ import './Menu.css'
 
 const Menu = ({ handleClass, clases, marcas }) => {
 
-  const [mostrar, setMostrar] = useState('');
+  const [mostrarCategoria, setMostrarCategoria] = useState(true);
+  const [mostrarMarca, setMostrarMarca] = useState(true);
+  const [mostrarPrecio, setMostrarPrecio] = useState(true);
   
-
-  const handleClick = (lista) => {
-
-    setMostrar(lista)
-
-    if (mostrar === lista) {
-      setMostrar('')
-    }
-
-  } 
-
   return (
     <div className="Menu">
-      <button className="Menu__filtros" onClick={() => handleClick('categoria')}>
+      <button className="Menu__filtros" onClick={() => setMostrarCategoria(!mostrarCategoria)}>
         <span>Categoría</span>
-        <FontAwesomeIcon icon={mostrar === 'categoria' ? faMinusCircle : faPlusCircle} />
+        <FontAwesomeIcon icon={mostrarCategoria ? faPlusCircle : faMinusCircle} />
       </button>
-      <div className="Menu__listas" hidden={ mostrar !== 'categoria' && true }>
+      <div className="Menu__listas" hidden={ mostrarCategoria }>
         <ul>
           <li onClick={handleClass}>Todo</li>
           {clases.map((item) => (
@@ -33,11 +24,11 @@ const Menu = ({ handleClass, clases, marcas }) => {
           ))}
         </ul>
       </div>
-      <button className="Menu__filtros" onClick={() => handleClick('marca')}>
+      <button className="Menu__filtros" onClick={() => setMostrarMarca(!mostrarMarca)}>
         <span>Marca</span>
-        <FontAwesomeIcon icon={mostrar === 'marca' ? faMinusCircle : faPlusCircle} />
+        <FontAwesomeIcon icon={ mostrarMarca ? faPlusCircle : faMinusCircle} />
       </button>
-      <div className="Menu__listas" hidden={ mostrar !== 'marca' && true }>
+      <div className="Menu__listas" hidden={ mostrarMarca }>
         <ul>
           <li onClick={handleClass}>Todo</li>
           {marcas.map((item) => (
@@ -45,11 +36,11 @@ const Menu = ({ handleClass, clases, marcas }) => {
           ))}
         </ul>
       </div>
-      <button className="Menu__filtros" onClick={() => handleClick('precio')}>
+      <button className="Menu__filtros" onClick={() => setMostrarPrecio(!mostrarPrecio)}>
         <span>Precio</span>
-        <FontAwesomeIcon icon={mostrar === 'precio' ? faMinusCircle : faPlusCircle} />
+        <FontAwesomeIcon icon={mostrarPrecio ? faPlusCircle : faMinusCircle} />
       </button>
-      <div className="Menu__listas" hidden={mostrar !== 'precio' && true}>
+      <div className="Menu__listas" hidden={mostrarPrecio}>
         <ul>
           <li><input onChange={ handleClass } id="0-100" type="checkbox" value="0-100"/><label htmlFor="0-100">$0-$100</label></li>
           <li><input onChange={ handleClass } id="100-1000" type="checkbox" value="100-1000"/><label htmlFor="100-1000">$100-$1000</label></li>
